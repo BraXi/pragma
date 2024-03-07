a8826e20e2f1a9dfdab4e97775913d0ac651d553
BraXi <6434152+BraXi@users.noreply.github.com>
2024-03-07 23:01:07 +0100

    `GL_GENERATE_MIPMAP_HINT` undefined

--- src/renderer_ogl1/r_image.c
+++ src/renderer_ogl1/r_image.c
@@ -698,7 +698,7 @@ qboolean R_UploadTexture32(unsigned* data, int width, int height, qboolean mipma
 
 	if (mipmap)
 	{
-		glHint(GL_GENERATE_MIPMAP_HINT, GL_NICEST); // FIXME: this should be done once in r_init
+		//glHint(GL_GENERATE_MIPMAP_HINT, GL_NICEST); // FIXME: this should be done once in r_init
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, gl_filter_min);
 		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, gl_filter_max);
 		glGenerateMipmap(GL_TEXTURE_2D);
